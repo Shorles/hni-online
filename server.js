a364800b9cb6c771a805503276f1a07240e55a6f
Shorles <charlesmasters13@gmail.com>
2025-07-13 22:48:34 -0300

    eee

--- server.js
+++ server.js
@@ -619,4 +619,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`)); 
+server.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
